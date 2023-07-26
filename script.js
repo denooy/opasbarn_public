bb3b8b85353326e30df23fe30aed468e20b10ba2
denooy <89561213+denooy@users.noreply.github.com>
2023-07-26 17:27:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ function attendClass() {
     document.getElementById("woodworkingclass").setAttribute("value", "Yes");
   }
 
+  function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+    x.className += " responsive";
+    } else {
+    x.className = "navbar";
+    }
+  }
+
+  function toggleDisplay() {
+    const listItems = document.querySelectorAll("li");
+    for (const listItem of listItems) {
+      listItem.style.display = listItem.style.display === "block" ? "none" : "block";
+    }
+    }  
+    document.querySelector("a").addEventListener("click", toggleDisplay);
